62f9fe3230dfacd10ee132c78c6ebc957df29e2e
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-16 15:01:18 -0700

    still trying to fix js branching errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
   // $("form#animal").submit(function(event) {
-    event.preventDefault();
+  event.preventDefault();
     $("#nameInput").submit(function(event) {
       var username = $("input#username").val();
       $(".username").text(username);
     })
 
     $("form#quiz").submit(function(event) {
-      var score = parseInt($("#animal").val());
+      var score = parseInt($("value#animal").val());
 
-        console.log ();
-      if (score === 4)
+      console.log ();
+      if (score < 4)
         $("#quizresult1").show(text);
         // var movie = parseInt($("select#movie").val();
-      } else } {
+        else if (score < 3)
         $("#quizresult2").show(text);
+      })
     });
-  });
+  
