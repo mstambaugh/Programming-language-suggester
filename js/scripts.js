dd0fb4bf1fb7efead444244f29626c03f2c5be99
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-16 15:52:39 -0700

    add README

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
   // $("form#animal").submit(function(event) {
-  event.preventDefault();
-    $("#nameInput").submit(function(event) {
+    $("form#nameInput").submit(function(event) {
       var username = $("input#username").val();
       $(".username").text(username);
     })
 
     $("form#quiz").submit(function(event) {
-      var score = parseInt($("value#animal").val());
+      var score = parseInt($("value#animal").val();
 
       console.log ();
       if (score < 4)
-        $("#quizresult1").show(text);
+        $("#quizresult1").append(text);
         // var movie = parseInt($("select#movie").val();
         else if (score < 3)
         $("#quizresult2").show(text);
+        event.preventDefault();
       })
     });
-  
