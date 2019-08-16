fe041c9b3cb7f97c8212403f6ea0d0e07a405983
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-16 13:52:59 -0700

    break everything trying to make else if work

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,18 @@
 $(document).ready(function() {
   // $("form#animal").submit(function(event) {
+    event.preventDefault();
     $("#nameInput").submit(function(event) {
-      event.preventDefault();
       var username = $("input#username").val();
       $(".username").text(username);
-      // $("#quizresult").show();
-    });
-    console.log ();
 
     $("form#quiz").submit(function(event) {
-      var score = parseInt($("select#animal").val());
-      // var movie = parseInt($("select#movie").val();
+        console.log ();
+      var quiz = parseInt($("value#animal").val());
+        if ('animal' === 4)
+        // var movie = parseInt($("select#movie").val();
 
-      if (score) {
-        var score = (animal);
-        if (score === 4 || score < 4) {
-        }
         $("#quizresult1").show();
-      }
+      }) else {}
+        $("#quizresult2").show();
     });
   });
