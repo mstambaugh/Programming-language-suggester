a8cd371e33e6dc893eed36af09c0ec7b55a84ce8
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-16 13:02:00 -0700

    work on scripts for else/if

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
   // $("form#animal").submit(function(event) {
-  $("#nameInput").submit(function(event) {
-    event.preventDefault();
-    var username = $("input#username").val();
-    $(".username").text(username);
-    $("#quizresult").show();
-  });
-});
+    $("#nameInput").submit(function(event) {
+      event.preventDefault();
+      var username = $("input#username").val();
+      $(".username").text(username);
+      // $("#quizresult").show();
+    });
+    console.log ();
 
-console.log ();
+    $("form#quiz").submit(function(event) {
+      var score = parseInt($("select#animal").val());
+      // var movie = parseInt($("select#movie").val();
+
+      if (score) {
+        var score = (animal);
+        if (score === 4 || score < 4) {
+        }
+        $("#quizresult1").show();
+      }
+    });
+  });
